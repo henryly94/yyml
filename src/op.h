526afa4d8e45909cd61ee075cd807bf49e24162e
Yiyang Li <henryly94@gmail.com>
2020-05-17 21:56:22 -0700

    Implement Conv2D (and Im2Col).

--- src/op.h
+++ src/op.h
@@ -154,10 +154,90 @@ Variable<Type>* MM(Variable<Type>* va, Variable<Type>* vb) {
   return result;
 }
 
-// template <Type>
-// Variable<Type>* Conv2D(Variable<Type>* input, Variable<Type>* kernel) {
-//
-//}
+template <typename Type>
+void FlattenBackward(Variable<Type>* v, Variable<Type>* result) {
+  for (int i = 0; i < result->grads_.total(); i++) {
+    v->grads_.data_[i] += result->grads_.data_[i];
+  }
+  Variable<Type>::factory::RemoveInstance(result);
+}
+
+template <typename Type>
+void Flatten(Variable<Type>* v, Variable<Type>* result) {
+  result->autograd_.backward_fn = std::bind(FlattenBackward<Type>, v, result);
+  result->autograd_.next = {&v->autograd_};
+  for (size_t i = 0; i < v->values_.total(); i++) {
+    result->values_.data_[i] = v->values_.data_[i];
+  }
+}
+
+template <typename Type>
+Variable<Type>* Flatten(Variable<Type>* v) {
+  TensorShape flatten_shape = {v->values_.total(), 1};
+  auto* result = Variable<Type>::factory::GetNewInstance(flatten_shape);
+  Flatten(v, result);
+  return result;
+}
+
+template <typename Type>
+void Im2ColBackward(Variable<Type>* image, Variable<Type>* kernel,
+                    Variable<Type>* result) {
+  size_t m = image->values_.shape().dims[0];
+  size_t n = image->values_.shape().dims[1];
+  size_t p = kernel->values_.shape().dims[0];
+  size_t q = kernel->values_.shape().dims[1];
+
+  for (size_t i = 0; i < (m - p + 1) * (n - q + 1); i++) {
+    for (size_t j = 0; j < p * q; j++) {
+      size_t row = j / q + i / (n - q + 1);
+      size_t col = j % q + i % (n - q + 1);
+      image->grads_.data_[row * n + col] +=
+          result->grads_.data_[i * (p * q) + j];
+    }
+  }
+  Variable<Type>::factory::RemoveInstance(result);
+}
+
+template <typename Type>
+void Im2Col(Variable<Type>* image, Variable<Type>* kernel,
+            Variable<Type>* result) {
+  size_t m = image->values_.shape().dims[0];
+  size_t n = image->values_.shape().dims[1];
+  size_t p = kernel->values_.shape().dims[0];
+  size_t q = kernel->values_.shape().dims[1];
+
+  result->autograd_.backward_fn =
+      std::bind(Im2ColBackward<Type>, image, kernel, result);
+  result->autograd_.next = {&image->autograd_};
+  for (size_t i = 0; i < (m - p + 1) * (n - q + 1); i++) {
+    for (size_t j = 0; j < p * q; j++) {
+      size_t row = j / q + i / (n - q + 1);
+      size_t col = j % q + i % (n - q + 1);
+      result->values_.data_[i * (p * q) + j] =
+          image->values_.data_[row * n + col];
+    }
+  }
+}
+
+template <typename Type>
+Variable<Type>* Im2Col(Variable<Type>* image, Variable<Type>* kernel) {
+  size_t m = image->values_.shape().dims[0];
+  size_t n = image->values_.shape().dims[1];
+  size_t p = kernel->values_.shape().dims[0];
+  size_t q = kernel->values_.shape().dims[1];
+
+  TensorShape shape = {(m - p + 1) * (n - q + 1), p * q};
+  auto* result = Variable<Type>::factory::GetNewInstance(shape);
+  Im2Col(image, kernel, result);
+  return result;
+}
+
+template <typename Type>
+Variable<Type>* Conv2D(Variable<Type>* input, Variable<Type>* kernel) {
+  auto* col = Im2Col<Type>(input, kernel);
+  auto* flatten_kernel = Flatten<Type>(kernel);
+  return MM<Type>(col, flatten_kernel);
+}
 
 template <typename Type>
 void ReLUBackward(Variable<Type>* v, Variable<Type>* result) {
